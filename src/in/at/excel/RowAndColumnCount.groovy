0581f57a8261e4e02c37b875f9f503089237bc91
solitaryheart <matthes.meier@unitybox.de>
2019-07-03 17:53:35 +0200

    Adding POI Jar -> Groovy Standalone Program col-row count

---
+++ src/in/at/excel/RowAndColumnCount.groovy
@@ -0,0 +1,38 @@
+package in.at.excel
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+
+/* -> here for demonstration -> Standalone programm
+-> better choice create a utility class
+ */
+
+class RowAndColumnCount {
+
+    static main(args){
+        def fis = new FileInputStream("C:\\udemy\\Groovy\\My_Project_Workspace\\src\\in\\at\\files\\TestData.xlsx")
+
+        //reference workbook
+        def workbook = new XSSFWorkbook(fis)
+
+        //reference sheet
+        def sheet = workbook.getSheet("Credentials")
+
+        //to find column count first go to row with headings !
+
+        def row = sheet.getRow(0)
+
+        //Columnn count = last cell number in row 0 = Headings
+
+        def colCount = row.getLastCellNum()
+
+        //Printing Column count
+        println("Column count is : $colCount")
+
+        //Get RowCount
+
+        def rowCount = sheet.getLastRowNum() +1
+
+        println("Row count is : $rowCount")
+    }
+}
